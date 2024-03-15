92ec37592a29d13261c70ee98584dc16cebbbb6d
gsy <gaoshengyu@xiaohongshu.net>
2024-03-15 20:30:58 +0800

    feat: udpate Page1

--- src/components/invitacion/pages/page1.vue
+++ src/components/invitacion/pages/page1.vue
@@ -2,14 +2,19 @@
   <div class="h-screen flex justify-center text-5xl" ref="page">
     <div class="flex flex-col items-center">
       <!-- 路过 -->
-      <img src="https://s21.ax1x.com/2024/03/14/pFgJC26.md.png" alt="pFgJC26.png" border="0" />
+      <!-- <img src="https://s21.ax1x.com/2024/03/14/pFgJC26.md.png" alt="pFgJC26.png" border="0" /> -->
       <!-- 聚合 -->
-      <img src="https://pic.imgdb.cn/item/65f436ea9f345e8d03b680b5.png" alt="Snipaste_2024-03-14_20-30-55.png">
-      <!-- 微博 -->
-      <img src="https://tvax3.sinaimg.cn/mw690/007RIY06gy1hnrxipcsgvj30rk0zy7wh.jpg" alt="Snipaste_2024-03-14_20-30-55">
-      <div class="text-3xl mt-8 text-stone-500">2024. 4. 27</div>
-      <div class="text-lg mt-1 text-stone-500">我们期待您的到来</div>
-      <div class="flex gap-2 mt-12 items-center justify-center text-stone-500 text-lg">
+      <div
+        class="h-[70vh] w-screen sm:w-[640px] bg-[url(https://pic.imgdb.cn/item/65f436ea9f345e8d03b680b5.png)] bg-cover bg-center"
+      ></div>
+      <div class="font-serif flex justify-center items-center gap-3 text-xl mt-5">
+        <div>锟 斤 拷</div>
+        <div class="text-lg">&</div>
+        <div>烫 烫 烫</div>
+      </div>
+      <div class="font-serif text-3xl mt-4 text-stone-500">2024 / 4 / 27</div>
+      <div class="font-serif text-lg mt-1 text-stone-500">山西阳泉红玺汇酒店</div>
+      <div class="flex gap-2 mt-4 items-center justify-center text-stone-500 text-lg">
         <div class="time-box">{{ time.day }}</div>
         <div>天</div>
         <div class="time-box">{{ time.hour }}</div>
