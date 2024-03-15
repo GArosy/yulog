4f55244899e7104e40d3d977c754c95ccf1e827b
gsy <gaoshengyu@xiaohongshu.net>
2024-03-15 20:04:27 +0800

    feat: image CDN test

--- src/components/invitacion/pages/page1.vue
+++ src/components/invitacion/pages/page1.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="h-screen flex justify-center text-5xl" ref="page">
     <div class="flex flex-col items-center">
+      <!-- 路过 -->
       <img src="https://s21.ax1x.com/2024/03/14/pFgJC26.md.png" alt="pFgJC26.png" border="0" />
+      <!-- 聚合 -->
+      <img src="https://pic.imgdb.cn/item/65f436ea9f345e8d03b680b5.png" alt="Snipaste_2024-03-14_20-30-55.png">
+      <!-- 微博 -->
+      <img src="https://tvax3.sinaimg.cn/mw690/007RIY06gy1hnrxipcsgvj30rk0zy7wh.jpg" alt="Snipaste_2024-03-14_20-30-55">
       <div class="text-3xl mt-8 text-stone-500">2024. 4. 27</div>
       <div class="text-lg mt-1 text-stone-500">我们期待您的到来</div>
       <div class="flex gap-2 mt-12 items-center justify-center text-stone-500 text-lg">
